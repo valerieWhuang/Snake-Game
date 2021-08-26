adb64466f5e396a5e34b79efc294326333673065
Valerie Huang <valeriewhuang@valeries-mbp.myfiosgateway.com>
2021-08-25 21:22:57 -0400

    set up server and connected elephantsql

---
+++ elephantsql.js
@@ -0,0 +1,20 @@
+const pg = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const conString = process.env.DB_URI;
+const client = new pg.Client(conString);
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    // >> output: 2018-08-23T14:02:57.117Z
+    client.end();
+  });
+});
